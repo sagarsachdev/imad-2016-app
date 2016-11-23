7cd39acc34954d4e7867ad400f5b21506dca0d85
sagarsachdev <sagarsachdev25@yahoo.com>
2016-11-23 15:25:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ submit.onclick = function(){
         //Not done yet 
     };
     //Make the request
-    request.open('GET','http://sagarsachdev.imad.hasura-app.io/submit-name/name=' + name,true);
+    request.open('GET','http://sagarsachdev.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
 };
